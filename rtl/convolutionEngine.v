ef99c8b3ef0b17b721a440334c2f97b665cd27c7
Daniel Lee <lungshingdaniel.lee@mail.utoronto.ca>
2026-06-10 12:54:00 -0400

    update rtl

--- rtl/convolutionEngine.v
+++ rtl/convolutionEngine.v
@@ -37,11 +37,26 @@ module windowReg(pixelOut2, pixelOut1, pixelOut0, clk, windowRegOut);
 		parameter PIXEL_WIDTH = 8;
 		parameter KERNEL_SIZE = 3;
 		
-		input wire [PIXEL_WIDTH - 1 : 0] pixelOutlive, pixelOut1, pixelOut0;
+		input wire [PIXEL_WIDTH - 1:0] pixelOutlive, pixelOut1, pixelOut0;
 		input wire clk;
 		output wire [KERNEL_SIZE * KERNEL_SIZE * PIXEL_WIDTH - 1:0] windowRegOut;
 		
-		reg [PIXEL_WIDTH - 1:0][PIXEL_WIDTH - 1:0];
+		reg [PIXEL_WIDTH - 1:0] window [0:KERNEL_SIZE - 1][0:KERNEL_SIZE - 1];
+		
+		always @(posedge clk)
+			begin
+				integer i;
+					for (i = 0; i < KERNEL_SIZE; i = i + 1)
+						for (j = 0; j < KERNEL_SIZE; j  = j + 1)
+			end 
+					
+
+			
+		
+		always @(posedge clk)
+			for (i = 0; i < KERNEL_SIZE; i = i+ 1)
+				
+		
 //MAC=====================================================================================================
 
 module mac ();
